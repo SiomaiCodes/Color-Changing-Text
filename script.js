c9386de956158803a9df3d09a04723f7f7c5dcd3
SiomaiCodes <mjlayague82@gmail.com>
2024-12-30 00:03:33 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,6 @@
-//change font color and text to red
- document.getElementById('red').onclick = function () {
-     document.getElementById("color").style.color = "red";
-     document.getElementById("color").innerText = "Red";
-}
-
-//change font color and text to blue
-document.getElementById('blue').onclick = function () {
-    document.getElementById("color").style.color = "blue";
-    document.getElementById("color").innerText = "Blue";
-}
-    
-//change font color and text to green    
-document.getElementById('green').onclick = function () {
-        document.getElementById("color").style.color = "green";
-        document.getElementById("color").innerText = "Green";
+document.querySelectorAll('button').forEach(function(button) {
+    button.onclick = function () {
+        document.querySelector("#color").style.color = button.dataset.color;
+        document.querySelector("#color").innerText = button.dataset.text;
     }
-    
-    
+})
